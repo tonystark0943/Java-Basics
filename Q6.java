bb41a15899b58e142e69d050869e29e77833f981
tonystark0943 <tonystark0943@gmail.com>
2024-12-13 09:04:42 +0530

    Add files via upload

--- Q6.java
+++ Q6.java
@@ -1,6 +1,7 @@
 public class Q6 {
 	public static void main(String args[]) {
-		System.out.println("ASCII value of is "+(int)args[0]);
+		
+		System.out.println("ASCII value of "+args[0]+" is "+(int)args[0].charAt(0));
 		
 	}
 }
